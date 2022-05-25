897b22f8e6f03b5d2a2f4a1091f12de885efc872
Kojo Appiedu <benadkojo@gmail.com>
2022-05-25 13:44:59 +0000

    second

--- base/models.py
+++ base/models.py
@@ -56,3 +56,16 @@ class Message(models.Model):
         
 
 
+
+class Message(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete= models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now= True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__ (self):
+        return self.body[0:50]
