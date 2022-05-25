fe6ad9beacdad69603caa3364437cf17bd43b980
Kojo Appiedu <benadkojo@gmail.com>
2022-05-25 15:20:44 +0000

    views

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Message, Room
+from .models import Message, Room , Topic, User
 
 admin.site.register(User)
 admin.site.register(Room)
--- base/views.py
+++ base/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
-from .models
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from  django.db.models import Q
+from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic, Message, User
+from .forms import Roomform, UserForm, MyUserCreationForm
 
-# Create your views here.
+
+#Login page
+def loginPage(request):
+    page='login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+
+        try:
+            user= user.objects
